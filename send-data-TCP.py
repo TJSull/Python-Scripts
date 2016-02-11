0ee0254b8f5e44b05ae35942f2ae9556511f1b72
TJSull <terrancesullivanjr@gmail.com>
2016-02-11 17:33:21 -0500

    Update send-data-TCP.py

--- send-data-TCP.py
+++ send-data-TCP.py
@@ -3,10 +3,10 @@ import sys
 
 BUF = 'ABCDE' * (2500)		#Data to be sent - ABCDE * 2500
 TCP_IP="74.125.224.72"		#Example IP - Google IP set in Example
-TCP_PORT=80				    #Example Port
+TCP_PORT=80			#Example Port
 
 try:
-	#TCP Socket
+    #TCP Socket
     sc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sc.connect((TCP_IP, TCP_PORT))
     sc.send(BUF + "\n")
